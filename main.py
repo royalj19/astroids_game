e61cf3a4b09968faea3339137fe7fac40cf3e3b6
royalj19 <royalj19@ymail.com>
2025-03-27 16:50:03 -0600

    fixed constant's'

--- main.py
+++ main.py
@@ -2,9 +2,20 @@ import pygame
 from constants import *
 
 def main():
+    pygame.init()
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    # Use pygame's display.set.mode() to get a new GUI window
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # Create (infinite) game loop
+    while True:
+        # fill screen with solid black color
+        screen.fill(0)
+        # refresh screen (last thing in loop)
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
